38466606f65d64ba97d45e13405344ff85f5964e
François Melchior <melcode>
2023-12-17 17:20:59 +0100

    update file grafbase.config

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,25 +1,28 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { graph, auth, config } from '@grafbase/sdk'
+const g = graph.Standalone()
 
-const User = g.model('User', {
-  name: g.string().length({min: 2, max: 20}),
-  email: g.string().unique(),
+
+const User = g.type('User', {
+  name: g.string(),
+  email: g.email(),
   avatarUrl: g.url(),
   description: g.string(),
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
-  profile : g.relation(() => Profile),
 })
 
-const Profile = g.model('Profile', {
-  username: g.string().length({min: 3}),
+const Profile = g.type('Profile', {
+  username: g.string(),
   description: g.string(),
   image: g.url(),
   liveSiteUrl: g.url(),
   githubUrl : g.url(),
-  category: g.string().search(),
+  category: g.string(),
   country: g.string(),
-  createdBy: g.relation(() => User)
+  createdBy: g.ref(User)
 })
+
+g.union('UserAndProfile', {User, Profile})
 
 
 export default config({
